77bbdd217e2ff2161c81bc461e500de915edb1cc
Legati0 <yannick.zimmermann@gmx.de>
2022-01-24 18:35:54 -1000

    cfg can now be used globally with -g, no need to have a cfg file in every project

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ## Description
 Simple script to generate makefiles, I may change some stuff if something is not working. 
 
-The special thing about this makefile generator is, that it saves the specified compiler and compiler flags, no need to always set them when creating a new makefile. For this the compiler and compiler flags are stored in `/installdir/makemake.cfg`.
+The special thing about this makefile generator is, that it saves the specified compiler, compiler flags, the name of the exe and even how the makefile should be called. No need to always set them when creating a new makefile. For this the params are globally stored in `/path/to/install/makemake.cfg` and locally in your working dir as `.makemake.cfg`. The global config will be used if there is no local config. A local config will only be generated when a var is set locally. If there is no global config, a new one is generated.
+
 ## Installation
 ```bash
 $ cd /usr/src/
@@ -14,22 +15,33 @@ To test if it's working:
 ```
 $ makemake help
 ```
+
 ## Usage
 ### Creating a makefile
 ```
 $ makemake
 ```
 ### Setting a specific compiler
-Standard is `g++`.
+Standard is `g++`. Add `-g` to set it globally.
 ```
 makemake -compiler <compilername>
 ```
 ### Setting specific compiler flags
-Standard are no flags.
+Standard are no flags. Add `-g` to set it globally.
 ```
 makemake -flags <flag1> <flag2> <flag3> ...
 ```
 To clear compiler flags, just specify no flags
 ```
 makemake -flags
+```
+### Setting name of built executable
+Standard is `run`. Add `-g` to set it globally.
+```
+makemake -exe <exe name>
+```
+### Setting name of generated makefile
+Standard is `Makefile`. Add `-g` to set it globally.
+```
+makemake -mfname <makefile name>
 ```
--- makemake.py
+++ makemake.py
@@ -2,33 +2,65 @@
 import sys
 import os
 
+def getPath(_global: bool) -> str:
+	if _global:
+		return os.path.join(sys.path[0], "makemake.cfg")
+	else:
+		return ".makemake.cfg"
 
-def parseConfig():
-	gxx = ""
-	cflags = ""
-	with open("makemake.cfg", "r") as f:
+
+def parseF(fname: str) -> dict:
+	d = {}
+	with open(fname, "r") as f:
 		for l in f.readlines():
 			if l.startswith("GXX"):
-				gxx = l.split("=")[1].strip()
+				d["GXX"] = l.split("=")[1].strip()
 			if l.startswith("CFLAGS"):
-				cflags = l.split("=")[1].strip()
-	return gxx, cflags
+				d["CFLAGS"] = l.split("=")[1].strip()
+			if l.startswith("EXE"):
+				d["EXE"] = l.split("=")[1].strip()
+			if l.startswith("MFILE"):
+				d["MFILE"] = l.split("=")[1].strip()
+	return d
+
+def parseConfig():
+	# if local cfg, get local it, else get global cfg 
+	lPath = getPath(False)
+	if os.path.isfile(lPath):
+		return parseF(lPath)
+	return parseF(getPath(True))
 
 
-def writeConfig(gxx, cflags):
-	with open("makemake.cfg", "w") as f:
-		f.write("GXX = {}\n".format(gxx))
-		f.write("CFLAGS = {}\n".format(cflags))
+def writeConfig(d: dict, _global: bool):
+	with open(getPath(_global), "w") as f:
+		f.write("GXX = {}\n".format(d["GXX"]))
+		f.write("CFLAGS = {}\n".format(d["CFLAGS"]))
+		f.write("EXE = {}\n".format(d["EXE"]))
+		f.write("MFILE = {}\n".format(d["MFILE"]))
 
 
-def setCompiler(compiler):
-	gxx, cflags = parseConfig()
-	writeConfig(compiler, cflags)
+def setCompiler(compiler: str, _global: bool):
+	cfg = parseConfig()
+	cfg["GXX"] = compiler
+	writeConfig(cfg, _global)
 
 
-def setFlags(flags):
-	gxx, cflags = parseConfig()
-	writeConfig(gxx, " ".join(flags))
+def setFlags(flags: list, _global: bool):
+	cfg = parseConfig()
+	cfg["CFLAGS"] = " ".join(flags)
+	writeConfig(cfg, _global)
+
+
+def setExeName(exe: str, _global: bool):
+	cfg = parseConfig()
+	cfg["EXE"] = exe
+	writeConfig(cfg, _global)
+
+
+def setMfileName(mfile: str, _global: bool):
+	cfg = parseConfig()
+	cfg["MFILE"] = mfile
+	writeConfig(cfg, _global)
 
 
 def getIncludes(fname):
@@ -36,15 +68,11 @@ def getIncludes(fname):
 	isMain = False
 	with open(fname, "r") as f:
 		for l in f.readlines():
-			#if fname == "3_4.cpp":
-			#	print(l)
-			#	print(l.startswith("#include \""))
 			if l.startswith("#include \""):
 				first = l.find("\"") + 1
 				includes.append(l[ first : l.find("\"", first)])
 			if l.startswith("int main("):
 				isMain = True
-	#print(includes)
 	return includes, isMain
 
 
@@ -64,38 +92,66 @@ def getDependencies():
 
 def generateMakefile():
 	deps = getDependencies()
-	gxx, cflags = parseConfig()
-	with open("Makefile", "w") as f:
-		f.write("GXX = {}\n".format(gxx))
-		f.write("CFLAGS = {}\n".format(cflags))
+	cfg = parseConfig()
+	with open(cfg["MFILE"], "w") as f:
+		f.write("GXX = {}\n".format(cfg["GXX"]))
+		f.write("CFLAGS = {}\n".format(cfg["CFLAGS"]))
 		f.write("main: " + " ".join(tup[1] for tup in deps) + "\n")
-		f.write("\t$(GXX) $(CFLAGS) -o run " + " ".join(tup[1] for tup in deps) + "\n")
+		f.write("\t$(GXX) $(CFLAGS) -o {} ".format(cfg["EXE"]) + " ".join(tup[1] for tup in deps) + "\n")
 		f.write("\n")
 		for dep in deps:
 			f.write(dep[1] + ": " + dep[0] + " " + " ".join(dep[2]) + "\n")
 			f.write("\t$(GXX) $(CFLAGS) -c " + dep[0] + "\n")
 			f.write("\n")
 		f.write("clean:\n")
-		f.write("\trm *.o run\n")
+		f.write("\trm *.o {}\n".format(cfg["EXE"]))
 
 
 if __name__ == "__main__":
-	if not os.path.isfile("makemake.cfg"):
-		writeConfig("g++", "")
+	argv = sys.argv	# one shouldn't modify sys.argv
+	argv.pop(0)	# pop of file path
 
-	if len(sys.argv) == 1:
+	# write global cfg, if it doesn't exist
+	if not os.path.isfile(getPath(True)):
+		writeConfig({"GXX": "g++", "CFLAGS": "", "EXE": "run", "MFILE": "Makefile"}, True)
+	
+	# check if should be global
+	if "-g" in argv:
+		_global = True
+		argv.remove("-g")
+	else:
+		_global = False
+	
+	# no args -> generate makefile
+	if not argv:	
 		generateMakefile()
-	elif sys.argv[1] == "-compiler":
-		if len(sys.argv) < 3:
+		exit(0)
+	
+	# pop of first arg
+	fArg = argv.pop(0)	
+	if fArg == "-compiler":
+		if not argv:	# no args left
 			print("err: expected string specifying compiler")
 			exit(1)
-		setCompiler(sys.argv[2])
-	elif sys.argv[1] == "-flags":
-		setFlags(sys.argv[2 : ] if len(sys.argv) > 2 else [])
-	elif sys.argv[1] == "help":
+		setCompiler(argv[0], _global)
+	elif fArg == "-exe":
+		if not argv:	# no args left
+			print("err: expected string specifying name of exe")
+			exit(1)
+		setExeName(argv[0], _global)
+	elif fArg == "-mfname":
+		if not argv:	# no args left
+			print("err: expected string specifying name of makefile (should be 'makefile' or 'Makefile')")
+			exit(1)
+		setMfileName(argv[0], _global)
+	elif fArg == "-flags":
+		setFlags(argv, _global)
+	elif fArg == "help":
 		print("makemake\t- generates a makefile")
-		print("makemake -compiler <name>\t- sets used compiler to <name>")
-		print("makemake -flags <name1> <name2> ...\t- sets used flags to all names that follow")
+		print("makemake -compiler <name> (-g)\t- sets used compiler to <name>")
+		print("makemake -exe <name> (-g)\t- sets name of built execultable to <name>")
+		print("makemake -mfname <name> (-g)\t- sets name of created makefile to <name>, should be 'makefile' or 'Makefile', but I won't stop you")
+		print("makemake -flags <name1> <name2> ... (-g)\t- sets used flags to all names that follow")
 		print("makemake help\t- prints help")
 	else:
 		print("err: unkown command")
